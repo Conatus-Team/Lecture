a0a2304901c1d3409bf9d4e4590b15debb002aaa
KimRiun <kimriun27@gmail.com>
2022-08-14 17:55:10 +0900

    feat: 카프카로 강의 자세히보기 전송

--- src/main/java/moine/domain/event/LectureDetailShown.java
+++ src/main/java/moine/domain/event/LectureDetailShown.java
@@ -2,29 +2,51 @@ package moine.domain.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import moine.domain.dto.DetailShowDto;
 import moine.domain.entity.LectureDetailShow;
 import moine.infra.AbstractEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 //@AllArgsConstructor
 public class LectureDetailShown extends AbstractEvent {
-
+//
 //    private Long id;
 //    private Long userId;
 //    private Long lectureId;
 //    private String category;
 //    private Integer clickCount;
-    private List<LectureDetailShow> detailShowList;
+//    private List<LectureDetailShow> detailShowList;
+    private List<DetailShowDto> detailShowDtoList = new ArrayList<DetailShowDto>();
 
 //    public LectureDetailShown(LectureDetailShow aggregate) {
 //        super(aggregate);
 //    }
-//
+
     public LectureDetailShown(List<LectureDetailShow> detailShowList) {
         super(detailShowList);
+        System.out.println("detailShowList.size() = " + detailShowList.size());
+        for (LectureDetailShow element: detailShowList) {
+            DetailShowDto dto = new DetailShowDto(
+                    element.getId(),
+                    element.getUser().getUserId(),
+                    element.getLectureCrawling().getLectureId(),
+                    element.getCategoryName(),
+                    element.getClickCount()
+            );
+
+            System.out.println("dto = " + dto);
+            detailShowDtoList.add(dto);
+        }
+
     }
+
+
 
     public LectureDetailShown() {
         super();
