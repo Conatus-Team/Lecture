5e345c33305f04a7f28dba2de8b91f99911b3da7
Jin Nam <developsick@gmail.com>
2022-08-28 05:44:43 +0900

    add/edit k8s configs

--- src/main/java/moine/config/kafka/KafkaProcessor.java
+++ src/main/java/moine/config/kafka/KafkaProcessor.java
@@ -6,8 +6,8 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
 
 public interface KafkaProcessor {
-    String INPUT = "input";
-    String OUTPUT = "output";
+    String INPUT = "event-in";
+    String OUTPUT = "event-out";
 
     @Input(INPUT)
     SubscribableChannel inboundTopic();
